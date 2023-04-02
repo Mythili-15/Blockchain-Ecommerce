2d7413ee2ac04f32eec3ded554c72f2c1ac23054
Mythili-15 <mythili.2005056@srec.ac.in>
2023-04-02 19:01:02 +0530

    ecommerce using blockchain

---
+++ client/src/components/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+    return <h1>Home</h1>;
+  };
+  
+  export default Products;
---
+++ server/data/Products.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Dress',
+    image: 'C:UsersmythiOneDrivePictures\123.png',
+    description: 'Amazing dress',
+    price: 500,
+    countInStock: 3,
+    rating: 4,
+    numReview: 4,
+  },
+];
